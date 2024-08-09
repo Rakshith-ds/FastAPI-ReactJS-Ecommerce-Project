8385f0cfc815641f1b5ffd7ab779e882b4370899
Rakshith <rakshith.ds10@gmail.com>
2024-08-08 19:43:21 -0500

    remove everything

--- src/theme/theme.js
+++
@@ -1,8 +0,0 @@
-import { createTheme } from "@mui/material/styles";
-import { purple } from "@mui/material/colors";
-
-export const theme = createTheme({
-  palette: {
-    secondary: purple,
-  },
-});
